9735d7fba5efcab44e8b68779bbec44cdffcbebb
centervalentine <centervalentine@gmail.com>
2025-07-14 15:57:23 -0400

    W01 Code Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,8 +12,20 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        // Step 1: Make an empty array with the specified length.
 
-        return []; // replace this return statement with your own
+        var result = new double[length];
+
+        // Step 2: Loop through the empty array to fill it with multiples of the number.
+        for (int i = 0; i < length; i++)
+        {
+            // Step 3: number * (i+1) the multiplier times the index + 1) 
+            // Step 4: replace array[i] with the result.
+            result[i] = number * (i + 1);
+        }
+// Step 5: Return the filled array.
+        return result;
+        
     }
 
     /// <summary>
@@ -29,5 +41,16 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Step 1:  Get the numbers that will be cut/rotated off the end of the list.
+        var cutOffData = data.GetRange(data.Count - amount, amount);
+
+        // Step 2:  Add the cut-off elements to the beginning of the list.
+        data.InsertRange(0, cutOffData);
+
+        // Step 3:  Delete the last 'amount' elements from the list.
+        data.RemoveRange(data.Count - amount, amount);
+
+        // Step 4:  Modified list is returned by reference to data
     }
 }
